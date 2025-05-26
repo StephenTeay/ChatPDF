f21ab6fa81c996de3809ea8b33e63d074cd27308
StephenTeay <127366131+StephenTeay@users.noreply.github.com>
2025-05-26 07:35:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def get_text_chunks(text):
     return chunks
 
 def get_vectorstore(text_chunks):
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-exp-03-07")
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
