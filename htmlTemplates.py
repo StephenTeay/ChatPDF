94112143a4f14b51e476f12db69758bc4f4cecbc
StephenTeay <127366131+StephenTeay@users.noreply.github.com>
2025-05-26 13:02:13 +0100

    Update htmlTemplates.py

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -37,7 +37,7 @@ bot_template = '''
 user_template = '''
 <div class="chat-message user">
     <div class="avatar">
-        <img src="https://i.ibb.co/rdZC7LZ/Photo-logo-1.png">
+        <img src="https://images.unsplash.com/photo-1747515203898-2df8f083f417?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
     </div>    
     <div class="message">{{MSG}}</div>
 </div>
